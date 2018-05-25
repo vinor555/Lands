72f87367e9d510cd7ec290dcab1bcf11bf9299d4
vinor555 <kevincoma555@gmail.com>
2018-05-24 19:25:20 -0600

    LoginPage

---
+++ lands/Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lands.ViewModels;
+namespace Lands.Infrastructure
+{
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
